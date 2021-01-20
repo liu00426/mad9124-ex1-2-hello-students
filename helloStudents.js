9c661ac761fca7589f61322d0f3666be0c292740
“liu00426” <liu00426@algonquinlive.com>
2021-01-21 04:25:51 +0800

    ex1

--- helloStudents.js
+++ helloStudents.js
@@ -1,9 +1,15 @@
-'use strict'
-
+"use strict"
 // 1. Read the JSON file into a variable called students
+const students = require("./students.json")
+const say = (message) => console.log(message)
 
+say("Hello world!")
+
+students.forEach(({ firstName, lastName }) => say(`Hello ${firstName} ${lastName}`))
+students.forEach((student) => say(`Hello ${student.firstName} ${student.lastName}`))
 // 2. Iterate over the students array and print Hello with their full names to the console
 // e.g. Hello Walter Baker
-
+const lastNameStartatD = students.filter(({ lastName }) => lastName.charAt(0) === "D")
+say(`Count of last names starting with D is ${lastNameStartatD.length + 1}`)
 // 3. Print out the number of last names starting with the letter D
 // e.g. Count of last names starting with D is 1
